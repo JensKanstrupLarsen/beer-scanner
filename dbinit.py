99188844b45bc53db2389a2a5b7554e6e0f8e50b
Jens Kanstrup Larsen <bgs509@alumni.ku.dk>
2018-08-15 15:46:42 +0200

    Init-fil til database

---
+++ dbinit.py
@@ -0,0 +1,18 @@
+import sqlite3 as sqlite
+import os
+import datetime
+
+if os.path.exists ("øllerbøller.db"):
+    os.rename ("øllerbøller.db", "backups/øllerbøller" + str(datetime.datetime.now()) + ".db")
+
+f = open("øllerbøller.db", "w").write("")
+
+conn = sqlite.connect("øllerbøller.db")
+c = conn.cursor()
+
+c.execute("CREATE TABLE bøller (id int, name text, faction text, level int)")
+c.execute("CREATE TABLE øller (id int, product text, price int)")
+c.execute("CREATE TABLE øllerbøller (bølle int, ølle int, dato datetimeqty int)")
+
+conn.commit()
+conn.close()
